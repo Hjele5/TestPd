6bb8385c6b8eebec6b21cbe42c57e27b7b79f469
Hjele5 <Hjele5@live.dk>
2016-04-27 14:13:05 +0100

    working song

---
+++ app/src/main/java/com/example/hjalte/testpd/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.hjalte.testpd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.io.PdAudio;
+import org.puredata.android.utils.PdUiDispatcher;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean weather = false;
+
+    private PdUiDispatcher dispatcher;
+
+    public void buttonOnClick(View v){
+        Button button=(Button) v;
+        ((Button)v).setText("clicked");
+
+    }
+
+
+    private void initPD() throws IOException{
+        int sampleRate = AudioParameters.suggestSampleRate();
+        PdAudio.initAudio(sampleRate,0,2,8,true);
+
+        dispatcher = new PdUiDispatcher();
+        PdBase.setReceiver(dispatcher);
+    }
+
+    private void initGUI1() {
+        Switch weatherSwitch = (Switch) findViewById(R.id.weatherSwitch);
+        weatherSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                boolean weather = true;
+            }
+        });
+    }
+
+    private void initGUI(){
+        Switch onOffSwitch = (Switch) findViewById(R.id.onOffSwitch);
+        onOffSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    float vul = (b) ? 1.0f : 0.0f;
+                    PdBase.sendFloat("songOn", vul);
+                    float val = (b) ? 1.0f : 0.0f;
+                    PdBase.sendFloat("onOff", val);
+                }});
+    }
+
+    private void loadPDPatch() throws IOException {
+        File dir = getFilesDir();
+        IoUtils.extractZipResource(getResources().openRawResource(R.raw.testpatch),dir,true);
+        File pdPatch = new File(dir,"testpatch.pd");
+        PdBase.openPatch(pdPatch.getAbsolutePath());
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            initPD();
+            loadPDPatch();
+        }catch (IOException e){
+            finish();
+        }
+        initGUI();
+        initGUI1();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        PdAudio.startAudio(this);
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        PdAudio.stopAudio();
+    }
+}
